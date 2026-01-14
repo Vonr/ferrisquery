d7d5888e185fc9a4c7a6e11cfcc0bde017f23405
Qther <qther@tuta.io>
2026-01-14 22:18:57 +0800

    chumsky to parse

--- src/interface.rs
+++ src/interface.rs
@@ -1,4 +1,5 @@
 use rcon::Result;
+use uuid_mc::PlayerUuid;
 
 type Connection = rcon::Connection<tokio::net::TcpStream>;
 
@@ -38,36 +39,78 @@ impl Interface {
     }
 
     pub async fn player_list(&mut self) -> std::result::Result<Vec<PlayerInfo>, crate::Error> {
+        use chumsky::prelude::*;
+
         let list_output = self.exec("list uuids").await?;
-        let mut list = Vec::with_capacity(list_output.bytes().filter(|&b| b == b',').count());
 
-        let Some((_, players)) = list_output.split_once("players online: ") else {
-            return Ok(list);
-        };
-
-        for p in players.split(", ") {
-            if p.is_empty() {
-                break;
-            }
-
-            let Some((name, uuid)) = p.rsplit_once(" (") else {
-                return Err(format!("Expected player information in form `name (uuid)` but could not find ` (` for {p:?} in {list_output}").into());
+        fn parser<'a>() -> impl Parser<'a, &'a str, Vec<PlayerInfo>, extra::Err<Rich<'a, char>>> {
+            let hex = |n| {
+                any()
+                    .filter(char::is_ascii_hexdigit)
+                    .repeated()
+                    .exactly(n)
+                    .to_slice()
+                    .try_map(|s: &str, span| {
+                        u128::from_str_radix(s, 16).map_err(|e| Rich::custom(span, e))
+                    })
             };
 
-            let Some((uuid, _)) = uuid.rsplit_once(")") else {
-                return Err(format!("Expected player information in form `name (uuid)` but could not find ending `)` for {p:?} in {list_output}").into());
-            };
+            group((
+                just("There are ").ignored(),
+                text::int(10).ignored(),
+                just(" of a max ").ignored(),
+                text::int(10).ignored(),
+                just(" players online: ").ignored(),
+                group((
+                    any()
+                        .filter(|b| *b != ' ')
+                        .repeated()
+                        .at_least(1)
+                        .to_slice(),
+                    just(' ').ignored(),
+                    group((
+                        hex(8),
+                        just('-').ignored(),
+                        hex(4),
+                        just('-').ignored(),
+                        hex(4),
+                        just('-').ignored(),
+                        hex(4),
+                        just('-').ignored(),
+                        hex(12),
+                    ))
+                    .try_map(|(a, _, b, _, c, _, d, _, e), span| {
+                        let mut uuid = a;
+                        uuid <<= 16;
+                        uuid += b;
+                        uuid <<= 16;
+                        uuid += c;
+                        uuid <<= 16;
+                        uuid += d;
+                        uuid <<= 48;
+                        uuid += e;
 
-            list.push(PlayerInfo {
-                name: name.into(),
-                uuid: uuid_mc::PlayerUuid::new_with_uuid(uuid_mc::Uuid::parse_str(uuid)?)?,
-            });
+                        PlayerUuid::new_with_uuid(uuid_mc::Uuid::from_u128(uuid))
+                            .map_err(|e| Rich::custom(span, e))
+                    })
+                    .delimited_by(just('('), just(')')),
+                ))
+                .map(|(name, _, uuid): (&str, (), _)| PlayerInfo {
+                    name: name.into(),
+                    uuid,
+                })
+                .separated_by(just(' '))
+                .collect::<Vec<_>>(),
+            ))
+            .map(|(_, _, _, _, _, info)| info)
         }
 
-        Ok(list)
+        let result = parser().parse(&list_output).into_result();
+        result.map_err(|e| format!("Failed to parse output of `/list uuids`: {:?}", e).into())
     }
 }
 
+#[derive(Clone)]
 pub struct PlayerInfo {
     pub name: Box<str>,
     pub uuid: uuid_mc::PlayerUuid,
